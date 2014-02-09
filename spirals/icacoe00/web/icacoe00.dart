9cf5b6df7d0e20aacdde903e64631e1b886cb62a
Dzenan Ridjanovic <dzenanr@gmail.com>
2014-02-09 11:34:37 -0500

    add spiral 00

---
+++ spirals/icacoe00/web/icacoe00.dart
@@ -0,0 +1,17 @@
+import 'dart:html';
+
+init() {
+  var canvas = querySelector('#canvas');
+  var context = canvas.getContext("2d");
+  drawRectangle(context, 0, 0, canvas.width, canvas.height);
+}
+
+drawRectangle(context, x, y, width, height) {
+  context
+      ..rect(x, y, width, height)
+      ..stroke();
+}
+
+main() {
+  init();
+}
