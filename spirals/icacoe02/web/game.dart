95201dc9d11b338657a3f438c9b9b44a501ba539
Dzenan Ridjanovic <dzenanr@gmail.com>
2014-02-20 17:43:20 -0500

    use every in assert

--- spirals/icacoe02/web/game.dart
+++ spirals/icacoe02/web/game.dart
@@ -7,9 +7,12 @@ main() {
   var grid = new SquareGrid(3);
   var cell23 = grid.cell(1, 2);
   assert(cell23.row == 1 && cell23.column == 2);
-  for (var c in grid.cells) assert(c.text == null);
+  //for (Cell c in grid.cells) assert(c.text == null);
+  assert(grid.cells.every((c) => c.text == null));
   // view
   new Board(grid, querySelector('#canvas')).draw();
 }
+
+
 
 
